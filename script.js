91fe304e52e4e1dcc6b605c9b8bb5b6bc4b043cd
Mohammed Abualnasr <hamadaelexboy@gmail.com>
2026-02-04 02:26:50 +0200

    add functionalities of filtering and disabling ectensions and refactor som html and css

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
 const extensionsContainer = document.querySelector(".extensions");
 const template = document.querySelector("#ext-temp");
-
+const allFilterEl = document.querySelector("#all");
+let extensionsData;
 fetch("./data.json")
   .then((res) => res.json())
   .then((res) => {
-    res.forEach((extData) => {
-      const clone = document.importNode(template.content, true);
-      const extImgEl = clone.querySelector(".ext-img");
-      const extNameEl = clone.querySelector(".ext-name");
-      const extDescriptionEl = clone.querySelector(".ext-description");
-      const activationStatusEl = clone.querySelector("input[type='checkbox']");
-      const activationLabelEl = clone.querySelector('label');
-      extImgEl.setAttribute("src", extData.logo);
-      extImgEl.setAttribute("alt", extData.name + " Logo");
-      activationStatusEl.setAttribute('name', extData.name);
-      activationStatusEl.setAttribute('id', extData.name);
-        activationLabelEl.setAttribute('for', extData.name);
-      extNameEl.textContent = extData.name;
-      extDescriptionEl.textContent = extData.description;
-      extensionsContainer.appendChild(clone);
-      if (extData.isActive) {
-        activationStatusEl.setAttribute("checked", extData.isActive);
+    extensionsData = res;
+    renderExtensions(res);
+  });
+
+function renderExtensions(data) {
+  extensionsContainer.innerHTML = "";
+
+  data.forEach((extData) => {
+    const clone = document.importNode(template.content, true);
+    const extEl = clone.querySelector(".extension");
+    const extImgEl = clone.querySelector(".ext-img");
+    const extNameEl = clone.querySelector(".ext-name");
+    const extDescriptionEl = clone.querySelector(".ext-description");
+    const activationStatusEl = clone.querySelector("input[type='checkbox']");
+    const activationLabelEl = clone.querySelector("label");
+    extImgEl.setAttribute("src", extData.logo);
+    extImgEl.setAttribute("alt", extData.name + " Logo");
+    activationStatusEl.setAttribute("name", extData.name);
+    activationStatusEl.setAttribute("id", extData.name);
+    activationLabelEl.setAttribute("for", extData.name);
+    extNameEl.textContent = extData.name;
+    extDescriptionEl.textContent = extData.description;
+    extensionsContainer.appendChild(clone);
+    activationStatusEl.checked = extData.isActive;
+
+    activationStatusEl.addEventListener("input", () => {
+      extData.isActive = !extData;
+      if (!allFilterEl.checked) {
+        extEl.style.display = "none";
       }
     });
   });
+}
+// Dark & light themes
 const root = document.documentElement;
 const themeToggler = document.querySelector(".color-mode-toggle");
 const themeIcon = document.querySelector(".mode-icon");
 themeToggler.addEventListener("click", () => {
-    if (root.getAttribute('data-theme') === 'dark'){
-        root.setAttribute("data-theme", "light");
-        themeIcon.setAttribute('src', './assets/images/icon-moon.svg')
-        themeIcon.setAttribute('alt', 'icon of light mode')
-    }
-    else{
-        root.setAttribute("data-theme", "dark");
-        themeIcon.setAttribute('src', './assets/images/icon-sun.svg')
-        themeIcon.setAttribute('alt', 'icon of light mode')
-    }
+  if (root.getAttribute("data-theme") === "dark") {
+    root.setAttribute("data-theme", "light");
+    themeIcon.setAttribute("src", "./assets/images/icon-moon.svg");
+    themeIcon.setAttribute("alt", "icon of light mode");
+  } else {
+    root.setAttribute("data-theme", "dark");
+    themeIcon.setAttribute("src", "./assets/images/icon-sun.svg");
+    themeIcon.setAttribute("alt", "icon of light mode");
+  }
 });
+
+// filtering functionality
+[...document.querySelectorAll("input[name=filter]")].forEach((filterEl) => {
+  filterEl.addEventListener("input", () => {
+    const filteredExtensions = filterExtensions(extensionsData, filterEl.id);
+    renderExtensions(filteredExtensions);
+  });
+});
+
+function filterExtensions(extensions, filter = null) {
+  let filteredExtensions;
+  switch (filter) {
+    case "active":
+      filteredExtensions = extensions.filter((extension) => {
+        return extension.isActive;
+      });
+      break;
+    case "inactive":
+      filteredExtensions = extensions.filter((extension) => {
+        return !extension.isActive;
+      });
+      break;
+
+    default:
+      filteredExtensions = extensions;
+      break;
+  }
+  return filteredExtensions;
+}
